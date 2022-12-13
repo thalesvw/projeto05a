8163ee005cb0c168819c470ae799ec7e7a51f518
Thales Gonçalves de Souza <78889415+thalesvw@users.noreply.github.com>
2022-12-13 01:47:42 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,40 @@
-const screen1 = document.querySelector(".screen1")
-const screen2 = document.querySelector(".screen2")
-const randomNumber = Math.round(Math.random() * 10)
-let xAttempts = 1
-function handleTryClick(event) {
-  event.preventDefault()
-  const inputNumber = document.querySelector("#inputNumber")
-  if (Number(inputNumber.value) == randomNumber) {
-    screen1.classList.add("hide")
-    screen2.classList.remove("hide")
-    screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas`
-  }
-  inputNumber.value=""
-  xAttempts++
-}
-
-// Eventos
-
 const btnTry = document.querySelector("#btnTry")
 const btnReset = document.querySelector("#btnReset")
-
-// Função callback
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+let randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1
 
 btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', function() {
-  screen1.classList.remove("hide")
-  screen2.classList.add("hide")
-  xAttempts = 1
-})
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', pressEnter)
+
+function handleTryClick(event) {
+event.preventDefault()
+const inputNumber = document.querySelector("#inputNumber")
+if (Number(inputNumber.value) == randomNumber) {
+toggleScreen()
+screen2.querySelector("h2").innerText = `Acertou em ${xAttempts} tentativas`
+}
+if(inputNumber.value != "") {
+xAttempts++
+}
+inputNumber.value = ""
+}
+
+function handleResetClick() {
+toggleScreen()
+xAttempts = 1
+randomNumber = Math.round(Math.random() * 10)
+}
+
+function toggleScreen() {
+screen1.classList.toggle("hide")
+screen2.classList.toggle("hide")
+}
+
+function pressEnter(e) {
+if (e.key == 'Enter' && screen1.classList.contains('hide')) {
+handleResetClick()
+}
+}
